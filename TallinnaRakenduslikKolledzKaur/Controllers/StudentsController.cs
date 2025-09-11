c6a58477f49380879c441fa896b431bd7235b67a
opilane <opilane@HP-CZC3217061>
2025-09-11 14:01:41 +0300

    Proovisin teha Clone, see ei tööta ja ma pole kindel miks.

--- TallinnaRakenduslikKolledzKaur/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledzKaur/Controllers/StudentsController.cs
@@ -44,16 +44,16 @@ namespace TallinnaRakenduslikKolledzKaur.Controllers
         /// <summary>
         ///  Get delete view for student
         /// </summary>
-        /// <param name="ID">id of student</param>
+        /// <param name="Id">id of student</param>
         /// <returns></returns>
         [HttpGet]
-        public async Task<IActionResult> Delete(int? ID)
+        public async Task<IActionResult> Delete(int? Id)
         {
-            if (ID == null)
+            if (Id == null)
             {
                 return NotFound();
             }
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == ID);
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == Id);
             if (student == null) 
             {
                 return NotFound();
@@ -62,21 +62,21 @@ namespace TallinnaRakenduslikKolledzKaur.Controllers
         }
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int? ID)
+        public async Task<IActionResult> DeleteConfirmed(int? Id)
         {
-            var student = await _context.Students.FindAsync(ID);
+            var student = await _context.Students.FindAsync(Id);
             _context.Students.Remove(student);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
         [HttpGet]
-        public async Task<IActionResult> Details(int? ID)
+        public async Task<IActionResult> Details(int? Id)
         {
-            if (ID == null)
+            if (Id == null)
             {
                 return NotFound();
             }
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == ID);
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == Id);
             if (student == null)
             {
                 return NotFound();
@@ -84,13 +84,13 @@ namespace TallinnaRakenduslikKolledzKaur.Controllers
             return View(student);
         }
                      [HttpGet]
-        public async Task<IActionResult>Edit(int? ID)
+        public async Task<IActionResult>Edit(int? Id)
         {
-            if (ID == null)
+            if (Id == null)
             {
                 return NotFound();
             }
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == ID);
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == Id);
             if (student == null)
             {
                 return NotFound();
@@ -107,27 +107,33 @@ namespace TallinnaRakenduslikKolledzKaur.Controllers
             return RedirectToAction("Index");
         }
         [HttpGet]
-        public async Task<IActionResult> Clone(int? ID)
+        public async Task<IActionResult> Clone(int? Id)
         {
-            if (ID == null)
+            if (Id == null)
             {
                 return NotFound();
             }
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == ID);
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == Id);
             if (student == null)
             {
                 return NotFound();
             }
-            _context.Students.Update(student);
             return View(student);
         }
         [HttpPost, ActionName("Clone")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CloneConfirmed([Bind("Id,LastName,FirstName,EnrollmentDate,Commendments")] Student student)
+        public async Task<IActionResult> CloneConfirmed(int? Id)
         {
-            _context.Students.Add(student);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == Id);
+            //ModelState.Remove("Id");
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+                // return RedirectToAction(nameof(Index))
+            }
+            return View(student);
         }
         /*
         [HttpPost, ActionName("Edit")]
